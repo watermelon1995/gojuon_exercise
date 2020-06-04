d10ae2db0aa9676695c67d7e90c757dbf606096e
watermelon1995 <qoo238302@gmail.com>
2020-06-04 23:44:49 +0800

    Updates

---
+++ precache-manifest.d419d72273637b741cab538be0fb4eeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35bd44041cfcac30569cf33a18ccdeb5",
+    "url": "/gojuon_exercise/index.html"
+  },
+  {
+    "revision": "2f9105f21dc44804ff3a",
+    "url": "/gojuon_exercise/static/css/main.07fa540b.chunk.css"
+  },
+  {
+    "revision": "9f480fdd315188384b60",
+    "url": "/gojuon_exercise/static/js/2.54f7b7cf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gojuon_exercise/static/js/2.54f7b7cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f9105f21dc44804ff3a",
+    "url": "/gojuon_exercise/static/js/main.5276dbf8.chunk.js"
+  },
+  {
+    "revision": "16aee79fc8ece6aa2ace",
+    "url": "/gojuon_exercise/static/js/runtime-main.e4a9a599.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gojuon_exercise/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d02a83791e398787f9734bd72c831659.js"
+  "/gojuon_exercise/precache-manifest.d419d72273637b741cab538be0fb4eeb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gojuon_exercise/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
